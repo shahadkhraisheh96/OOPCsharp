6a661e87a6438d39421f2186b71bfcf507a1bb62
shahadkhraisheh96 <shahadkh2004@gmail.com>
2026-04-20 14:47:41 +0300

    task 2 oop

---
+++ oopTask2/oopTask2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace oopTask2 {
+
+    class Car
+    {
+        public int Year { get; set; }
+        public string Type { get; set; }
+        public double Price { get; set; }
+        public string Model { get; set; }
+        public string PalletNo { get; set; }
+        public string Color { get; set; }
+        public static bool Engine = false;
+        public Car(int year, string type, double price, string model, string palletNo, string color)
+        {
+            this.Year = year;
+            this.Type = type;
+            this.Price = price;
+            this.Model = model;
+            this.PalletNo = palletNo;
+            this.Color = color;
+            Engine = false;
+        }
+        public bool StartEngine()
+        {
+            Engine = true;
+            return Engine;
+        }
+        public bool StopEngine()
+        {
+            Engine = false;
+            return Engine;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Car Details \n" +
+               $"Year: {Year}\n" +
+               $"Type: {Type}\n" +
+               $"Price: ${Price}\n" +
+               $"Pallet No: {PalletNo}\n" +
+               $"Color: {Color}");
+        }
+
+   
+    public static void Main()
+        {
+            Car myCar = new Car(2024, "Electric Sedan", 45000.00, "Model 3", "ABC-1234", "Deep Blue");
+
+            myCar.Print();
+
+
+
+            myCar.StartEngine();
+            myCar.StopEngine();
+        }
+
+    }
+}
+
+
