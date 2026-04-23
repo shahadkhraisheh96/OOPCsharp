968c417ccb2c08c81e06d53446e1620508f70fad
shahadkhraisheh96 <shahadkh2004@gmail.com>
2026-04-23 16:01:01 +0300

    file task

---
+++ FileHandling&Library/FileHandling&Library/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHandling_Library
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                String[] lines = File.ReadAllLines("C:\\Users\\user\\OOPCsharp\\FileHandling&Library\\FileHandling&Library\\data.txt");
+              
+                File.AppendAllText( "C:\\Users\\user\\OOPCsharp\\FileHandling&Library\\FileHandling&Library\\data.txt", "\n hello word");
+
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+
+            }
+            catch(Exception e) {
+            
+            Console.WriteLine(e.ToString());
+            
+            }
+
+        }
+    }
+}
