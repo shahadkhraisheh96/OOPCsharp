d0273e5d2ddd29273b22139cb8b58a736864cbb7
shahadkhraisheh96 <shahadkh2004@gmail.com>
2026-04-25 19:52:11 +0300

    all

--- FileHandling&Library/FileHandling&Library/Program.cs
+++ FileHandling&Library/FileHandling&Library/Program.cs
@@ -17,6 +17,7 @@ namespace FileHandling_Library
               
                 File.AppendAllText( "C:\\Users\\user\\OOPCsharp\\FileHandling&Library\\FileHandling&Library\\data.txt", "\n hello word");
 
+
                 foreach (string line in lines)
                 {
                     Console.WriteLine(line);
@@ -29,6 +30,49 @@ namespace FileHandling_Library
             
             }
 
+            try
+            {
+                string filePath = "C:\\Users\\user\\OOPCsharp\\FileHandling&Library\\FileHandling&Library\\UserInfo.txt";
+                string name = "User";
+                string specialization = "Full-Stack Web Development";
+                string age = "25";
+                string description = "I specialize in frontend engineering and database management, focusing on responsive layouts.";
+
+                string[] linesToWrite = {
+            $"Name: {name}",
+            $"Specialization: {specialization}",
+            $"Age: {age}",
+            $"Description: {description}"
+        };
+
+                File.WriteAllLines(filePath, linesToWrite);
+
+                int totalCharactersNoSpaces = 0;
+                int totalWords = 0;
+
+
+                foreach (string line in File.ReadLines(filePath))
+                {
+                    Console.WriteLine(line);
+
+                     totalCharactersNoSpaces += line.Count(c => !char.IsWhiteSpace(c));
+
+                    string[] words = line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                    totalWords += words.Length;
+                }
+
+                Console.WriteLine($"Total characters (no spaces): {totalCharactersNoSpaces}");
+                Console.WriteLine($"Total words: {totalWords}");
+
+            }
+            catch (Exception e) {
+
+                Console.WriteLine(e.ToString());
+
+
+            }
+
         }
+        
     }
 }
